50a42ed22108a9e052c51a95978ea30b6f56f53c
arthurgeay <arthurgeay.contact@gmail.com>
2018-03-19 12:15:18 +0100

    Ajout d'effet de scroll pour la navigation

--- index.php
+++ index.php
@@ -10,31 +10,31 @@
     <link rel="stylesheet" href="css/circle.css">
     <title>Arthur Geay - Portfolio</title>
   </head>
-  <body>
+  <body id="page-top">
       <header>
         <div id="background">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
             <div class="container">
-              <a class="navbar-brand" href="#">Arthur Geay</a>
+              <a class="navbar-brand" href="#page-top">Arthur Geay</a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#about">A propos <span class="sr-only">(current)</span></a>
+                  <li class="nav-item js-scroll-trigger">
+                    <a class="nav-link" href="#about">A propos</a>
                   </li>
-                  <li class="nav-item">
+                  <li class="nav-item js-scroll-trigger">
                     <a class="nav-link" href="#resume">Mon parcours</a>
                   </li>
-                  <li class="nav-item">
+                  <li class="nav-item js-scroll-trigger">
                     <a class="nav-link" href="#skills">Compétences</a>
                   </li>
-                  <li class="nav-item">
+                  <li class="nav-item js-scroll-trigger">
                     <a class="nav-link" href="#project">Réalisations</a>
                   </li>
-                  <li class="nav-item">
+                  <li class="nav-item js-scroll-trigger">
                     <a class="nav-link" href="#contact">Contact</a>
                   </li>
                 </ul>
@@ -452,5 +452,6 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <script src="js/gallery.js"></script>
     <script src="js/notify.js"></script>
+    <script src="js/scrolling-nav.js"></script>
   </body>
 </html>
