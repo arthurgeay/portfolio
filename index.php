e62d0c5c70a6d632a4b3b3241b41ca23f660cfda
arthurgeay <arthurgeay.contact@gmail.com>
2018-06-05 18:40:21 +0200

    Modifs menu

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once('parameters.php');
     <meta name="description" content="Arthur Geay. Développeur web PHP/Symfony & Chef de projet">
     <meta name="keywords" content="developpeur, web, developpeur web, arthur geay, Arthur Geay, php, sql, symfony, javascript, bootstrap, gestion de projet, freelance">
     <link rel="icon" href="img/favicon.png" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/circle.css">
@@ -21,13 +21,13 @@ require_once('parameters.php');
         <div id="background">
           <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
             <div class="container">
-              <a class="navbar-brand" href="#page-top">Arthur Geay</a>
+              <a class="navbar-brand lbrand" href="#page-top">Arthur Geay</a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto">
+                <ul class="navbar-nav cmenu">
                   <li class="nav-item js-scroll-trigger">
                     <a class="nav-link" href="#about">A propos</a>
                   </li>
@@ -42,6 +42,9 @@ require_once('parameters.php');
                   </li>
                   <li class="nav-item js-scroll-trigger">
                     <a class="nav-link" href="#contact">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="btn btn-outline-primary navbar-btn" href="https://blog.arthurgeay.fr">Blog</a>
                   </li>
                 </ul>
               </div>
@@ -478,8 +481,8 @@ require_once('parameters.php');
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <script src="js/gallery.js"></script>
     <script src="js/notify.js"></script>
